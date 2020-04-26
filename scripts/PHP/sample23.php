43de28495bf440a6d2affb01564bf3b17e8c13dd
飛田大二郎 <pelo.canvas@gmail.com>
2020-04-26 23:46:42 +0900

    add sample23

---
+++ scripts/PHP/sample23.php
@@ -0,0 +1,20 @@
+<?php
+$file = $_FILES['my_img'];
+print('file name:'.$flie['name'].'<br />');
+print('file type:'.$flie['type'].'<br />');
+print('upload file:'.$flie['tmp_name'].'<br />');
+print('error:'.$file['error']);
+print('size:'.$file['size'].'<br />');
+
+$ext = substr($file['name'], -4);
+if($ext == '.gif' || $ext == '.jpg' || $ext=='.png')
+{
+    $filePath = './user_img'.$file['name'];
+    move_uploaded_file($file['tmp_name'], $filePath);
+    print('<img src="'.$filePath.'"/>');
+}
+else
+{
+    print('extension error');
+}
+?>
