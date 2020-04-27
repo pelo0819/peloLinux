6e77ffa6a8906a5a98ef10108210daffe2e0287d
飛田大二郎 <pelo.canvas@gmail.com>
2020-04-27 23:08:22 +0900

    re sample23

--- scripts/PHP/sample23.php
+++ scripts/PHP/sample23.php
@@ -1,10 +1,12 @@
 <?php
+
 $file = $_FILES['my_img'];
-print('file name:'.$flie['name'].'<br />');
-print('file type:'.$flie['type'].'<br />');
-print('upload file:'.$flie['tmp_name'].'<br />');
-print('error:'.$file['error'].'<br />');
-print('size:'.$file['size'].'<br />');
+
+print('file name:' . $flie['name'] .'<br />');
+print('file type:'. $flie['type'] .'<br />');
+print('upload file:'. $flie['tmp_name'] .'<br />');
+print('error:'. $file['error'] .'<br />');
+print('size:'. $file['size'] .'<br />');
 
 $ext = substr($file['name'], -4);
 if($ext == '.gif' || $ext == '.jpg' || $ext=='.png')
