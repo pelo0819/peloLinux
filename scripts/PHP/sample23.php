1064d592c0c0856dba84886b3c0d8705453792d8
飛田大二郎 <pelo.canvas@gmail.com>
2020-04-27 22:09:17 +0900

    change smaple23

--- scripts/PHP/sample23.php
+++ scripts/PHP/sample23.php
@@ -3,13 +3,13 @@ $file = $_FILES['my_img'];
 print('file name:'.$flie['name'].'<br />');
 print('file type:'.$flie['type'].'<br />');
 print('upload file:'.$flie['tmp_name'].'<br />');
-print('error:'.$file['error']);
+print('error:'.$file['error'].'<br />');
 print('size:'.$file['size'].'<br />');
 
 $ext = substr($file['name'], -4);
 if($ext == '.gif' || $ext == '.jpg' || $ext=='.png')
 {
-    $filePath = './user_img'.$file['name'];
+    $filePath = './user_img/'.$file['name'];
     move_uploaded_file($file['tmp_name'], $filePath);
     print('<img src="'.$filePath.'"/>');
 }
