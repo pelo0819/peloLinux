551b6918031618676129e1e5f0be110888868e36
飛田大二郎 <pelo.canvas@gmail.com>
2020-04-28 22:51:44 +0900

    add

--- scripts/PHP/sample23.php
+++ scripts/PHP/sample23.php
@@ -1,23 +1,15 @@
+
 <?php
-
-$file = $_FILES['my_img'];
-
-print('file name:' . $flie['name']);
-print('file type:'. $flie['type']);
-print('upload file:'. $flie['tmp_name']);
-print('error:'. $file['error']);
-print('size:'. $file['size']);
-
-$ext = substr($file['name'], -4);
-if($ext == '.gif' || $ext == '.jpg' || $ext=='.png')
-{
-    $filePath = './user_img/'.$file['name'];
-    print('filePath='.$filePath.'');
-    move_uploaded_file($file['tmp_name'], $filePath);
-    print('<img src="'.$filePath.'"/>');
-}
-else
-{
-    print('extension error');
-}
+$tempfile = $_FILES['fname']['tmp_name'];
+$filename = './' . $_FILES['fname']['name'];
+ 
+if (is_uploaded_file($tempfile)) {
+    if ( move_uploaded_file($tempfile , $filename )) {
+	echo $filename . "をアップロードしました。";
+    } else {
+        echo "ファイルをアップロードできません。";
+    }
+} else {
+    echo "ファイルが選択されていません。";
+} 
 ?>
--- scripts/PHP/sample23_input.php
+++ scripts/PHP/sample23_input.php
@@ -1,15 +1,4 @@
-<?php
-    var_dump($_FILES);
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-    <head><meta charset="utf-8"><title>テスト</title></head><body>
-        <form method="post" action="sample23.php" enctype="multipart/form-data">				
-    <input type="file" name="upload_file">		
-    <div>			
-        <input type="submit" name="btn_submit" value="送信">		
-    </div>	
-    </form>
-    </body>
-</html>
+<form action="sample23.php" method="post" enctype="multipart/form-data">
+  <input type="file" name="fname">
+  <input type="submit" value="アップロード">
+</form>
