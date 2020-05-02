f7b134a95269bac281f9df1f094207c751f46510
pelo0819 <canvas.pelo@gmail.com>
2020-05-02 22:34:38 +0900

    さらに変更

--- scripts/PHP/sample23_input.php
+++ scripts/PHP/sample23_input.php
@@ -1,4 +1,4 @@
-<form action="/home/pelo/Documents/peloLinux/scripts/PHP/canvasPHP/web/" method="post" enctype="multipart/form-data">
+<form action="/php/canvasPHP/web/index.php" method="post" enctype="multipart/form-data">
   <input type="file" name="fname">
   <input type="submit" value="アップロード">
 </form>
