77ca12ab83330d6148ff795be85e4642c6a510bd
飛田大二郎 <pelo.canvas@gmail.com>
2020-05-02 09:17:00 +0900

    connectMySQL.phpにデータベース作成テストを追加

--- scripts/PHP/perfectPHP/chapter6/connectMySQL.php
+++ scripts/PHP/perfectPHP/chapter6/connectMySQL.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysql_connect('localhost', 'root', '0819Tobita');
+    $link = mysql_connect('192.168.3.50', 'pelo', '0819Tobita');
     if($link)
     {
         echo 'connect database successfully'."\n";
@@ -8,4 +8,18 @@
     {
         die('cannot connect database.'. mysql_error());
     }
+
+    mysql_select_db('canvasdb');
+
+    $user = 'neko';
+    $pass = '0104kimura';
+    $addr = 'eri@gamil.com';
+
+    // 保存データを作成
+    $sql = 'insert into userinfo values('.$user.','.$pass.','.$addr.')';
+
+    // データを保存
+    mysql_query($sql, $link);
+
+    echo 'データを保存しました'."\n";
 ?>
