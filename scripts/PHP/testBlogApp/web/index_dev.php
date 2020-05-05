dc9f07d3a859ea73a8ab35bdd926f0dd1fa479ac
root <root@www.catkuma.com>
2020-05-05 12:39:30 +0900

    change

--- scripts/PHP/testBlogApp/web/index_dev.php
+++ scripts/PHP/testBlogApp/web/index_dev.php
@@ -1,6 +1,7 @@
 <?php
 
-require '../bootstrap.php';
+//require '../bootstrap.php';
+require '/home/pelo/Documents/peloLinux/scripts/PHP/testBlogApp/bootstrap.php';
 require '../MiniBlogApplication.php';
 
 $app = new MiniBlogApplication(true);
