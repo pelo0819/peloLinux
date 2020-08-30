e0662ade5a450895c441ee2212713d95e1e28bdd
pelo0819 <canvas.pelo@gmail.com>
2020-08-30 19:33:05 +0900

    fx

--- scripts/c/peloet/arp_poison.h
+++ scripts/c/peloet/arp_poison.h
@@ -12,6 +12,6 @@ void *PoisonTest(void * arg);
 
 void SetOptArpThread(pthread_attr_t _attr, pthread_t _thread_id);
 
-void TransferPacket(struct ether_header *eh, u_int8_t *data, int len, u_int8_t *all_data, int len_all)
+void TransferPacket(struct ether_header *eh, u_int8_t *data, int len, u_int8_t *all_data, int len_all);
 
 int maccmp(u_int8_t from_mac[6], u_int8_t to_mac[6]);
